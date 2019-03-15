9cf2254da7aebf83371e686c5351fb368ed164aa
Greyflanel <richardlovelace811@gmail.com>
2019-03-14 19:13:24 -0500

    I have created the objects and I am in the process of writing method calls to test it out.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -3,8 +3,8 @@
 class Person {
   constructor(attributes) {
     this.name = attributes.name;
-    this.age = attributes.age;
     this.location = attributes.location;
+    this.age = attributes.age;
     this.gender = attributes.gender;
   }
   speak() {
@@ -58,3 +58,91 @@ class ProjectManager extends Instructor {
     console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
   }
 }
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Back-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  const wilma = new Instructor({
+    name: 'Wilma',
+    location: 'Bedrock',
+    age: 32,
+    gender: 'female',
+    favLanguage: 'HTML',
+    specialty: 'Front-end',
+    catchPhrase: `Ride or Die`
+  });
+
+  const barney = new Instructor({
+    name: 'Barney',
+    location: 'Bedrock',
+    age: 35,
+    gender: 'male',
+    favLanguage: 'CSS',
+    specialty: 'Front-end',
+    catchPhrase: `Don't hate the playa, hate the game.`
+  });
+
+  const pebbles = new Student({
+      name: 'Pebbles',
+      location: 'Bedrock',
+      age: 2,
+      gender: 'female',
+      previousBackground: 'Baby',
+      className: 'Web32',
+      favSubjects: [' ABCs', ' Numbers', ' Stickers'] 
+  })
+
+  const bamBam = new Student({
+    name: 'BamBam',
+    location: 'Bedrock',
+    age: 3,
+    gender: 'male',
+    previousBackground: 'Baby',
+    className: 'Web39',
+    favSubjects: [' Girls', ' Rock-Ball', ' Lunch'] 
+})
+
+const betty = new Student({
+    name: 'Betty',
+    location: 'Bedrock',
+    age: 32,
+    gender: 'female',
+    previousBackground: 'Cave-wife',
+    className: 'Web18',
+    favSubjects: [' HTML', ' CSS', ' JavaScript'] 
+})
+
+const dino = new ProjectManager({
+    name: 'Dino',
+    location: 'Bedrock',
+    age: 10,
+    gender: 'male',
+    gradClassName: 'Self-taught',
+    faveInstructor: 'Sean'
+})
+
+const saurus = new ProjectManager({
+    name: 'Saurus',
+    location: 'Bedrock',
+    age: 15,
+    gender: 'male',
+    gradClassName: 'CS1',
+    faveInstructor: 'Josh'
+})
+
+const mammalia = new ProjectManager({
+    name: 'Mammalia',
+    location: 'Under-Bedrock',
+    age: 20,
+    gender: 'female',
+    gradClassName: 'N/A',
+    faveInstructor: 'Sean'
+})
+bamBam.listsSubjects();
